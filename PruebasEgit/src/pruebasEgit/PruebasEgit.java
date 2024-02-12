009000a00865b05a4c99ef9451568aa1d297c6b4
coral <coralf2004@gmail.com>
2024-02-12 09:46:05 +0100

    Primer commit rama1

--- PruebasEgit/src/pruebasEgit/PruebasEgit.java
+++ PruebasEgit/src/pruebasEgit/PruebasEgit.java
@@ -5,6 +5,7 @@ public class PruebasEgit {
 	public static void main(String[] args) {
 		System.out.println("PruebasEgit");
 		System.out.println("Segundo commit master");
+		System.out.println("Primer commit rama1");
 
 	}
 
